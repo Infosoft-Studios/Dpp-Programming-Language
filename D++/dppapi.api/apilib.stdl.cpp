d5f59e46acd2d13a1f0b63b33e358fcae9943ca2
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-06 21:11:54 +0200

    Create apilib.stdl.cpp

---
+++ D++/dppapi.api/apilib.stdl.cpp
@@ -0,0 +1,28 @@
+enum structbuffer
+{
+  longdat
+};
+
+struct data
+{
+    void *BusFile
+};
+
+void mna()
+{
+    // simulate system eng
+    return 0;
+}
+
+#include <windows.h>
+using namespace std;
+
+
+void ma_api()
+{
+    WINCLASS wc;
+
+    wc.getchar* Buffer(structbuffer::longdat)
+};
+
+
