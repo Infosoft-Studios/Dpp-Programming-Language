7aca1cdcee9971f0f5a7ccd13eaf16f9fb36f60d
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-06 20:25:43 +0200

    Update configuration_loader_compiler.c

---
+++ D++/lang/configuration_loader_compiler.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <windows.h>
+// include CompiledBaseprefix
+#include "compiledbaseprefix.cpp"
+
+
+typedef struct CompiledTokenReaction
+{
+    WINCLASS wc;
+
+    enum Token
+    {
+        Type,
+        Argument,
+        InputArgument,
+        TokenArgument,
+        EndResult // which is the Output
+    };
+
+    char praser_errorcodes[]                        = {'G', 'I', 'N', 'B', 'Q'}; // error codes
+    praser_errorcodes[0]                                         = 'G'
+
+    if (WINDOWSWIN::prazzer == praser_errorcode[0])
+    {
+        printf("Error in praser 1, more error will be compiled further");
+    };
+
+    wc.loadpart                             = LoadInitializeFunctions(NULL, 0);
+};
