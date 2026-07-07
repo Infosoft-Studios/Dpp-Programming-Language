b7ac2a12dd86fc74128f2457ca33d34cae0a9d4e
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-07 15:55:58 +0200

    Create lindisk.ld.c

---
+++ D++/kernaccsesoldwin/.oke/lindisk.ld.c
@@ -0,0 +1,15 @@
+typedef struct load
+{
+    void * buffer();
+    enum load
+    {
+        LoadTypeIntFunc,
+        SystLoadEx,
+        InitLoader
+    };
+
+    void getcharbuffer()
+    {
+        load::LoadTypeIntFunc;
+    }
+}
