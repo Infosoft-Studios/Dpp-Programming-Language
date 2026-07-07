bb75d9ba81bba9282aa032341d2144872d724978
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-07 18:57:23 +0200

    Update loadsys-vscode.bash

--- D++/dpp-vscode/loadsys-vscode.bash
+++ D++/dpp-vscode/loadsys-vscode.bash
@@ -1,11 +1,5 @@
-# 1. Go into your extension folder
 cd dpp-vscode
-
-# 2. Initialize a Node.js package
 npm init -y
-
-# 3. Install VS Code extension packaging tools
 npm install --save-dev @vscode/vsce
-
-# 4. (Optional) Install Yeoman + VS Code generator
-npm install -g yo generator-code
+npx vsce package
+code --install-extension *.vsix
