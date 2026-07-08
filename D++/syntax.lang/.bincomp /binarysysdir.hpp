f62938ab5580e92c9dfb4c4b6142a9041ccab796
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-08 19:02:06 +0200

    Correct syntax in loadbinaryconferences function
    Fix syntax errors in loadbinaryconferences function.

--- D++/syntax.lang/.bincomp /binarysysdir.hpp
+++ D++/syntax.lang/.bincomp /binarysysdir.hpp
@@ -7,6 +7,14 @@ void loadbinaryconferences(string SysDirDirPath)
 {
     WINCLASS wc;
     wc.sysdir              = LoadSysdir(true , SysDirDirPath);
+    wc.loadinitiailyzer    = FunctionSys(true, nullptr);
+    swicth (!wc.loadfunctions)
+    {
+        case wc.loader || wc.functioninitializer:
+            break;
+        default:
+            break;
+    };
 };
 
 int main()
