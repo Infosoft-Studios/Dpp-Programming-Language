e194639ef0f644c89d620b2afebc3793f205cce5
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-08 11:15:52 +0200

    Add nsloadsrc.cpp with initial loadnline structure

---
+++ D++/.packages/nsloadsrc.cpp
@@ -0,0 +1,16 @@
+struct loadnline
+{
+    void system()  
+    {
+        void* bufload();
+    };
+}
+
+#include <windows.h>
+
+class pro
+{
+    protected:
+        WINCLASS wc;
+        wc.loadinitializer                  = LoadFunctionalAspects(nullptr, "/#%sys%#/");
+};
