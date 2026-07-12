af1e5af6a5717343422cf3a3c5a26b7fa1b38528
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-12 18:30:07 +0200

    Create rstyle.cpp for token elimination functionality
    Add rstyle.cpp with initial implementation for token elimination.

---
+++ D++/syntax.lang/.bincomp /System32 /rstyle.cpp
@@ -0,0 +1,12 @@
+#include <windows.h>
+
+DWORD x32 = reintepeter_cast<void*> buf();
+DWORD x86_64 = reintepeter_cast<void*> FuncLoad();
+
+void tokeneliminate()
+{
+    enum token_load {
+        TokenSys,
+        ReLoadSys
+    };
+};
