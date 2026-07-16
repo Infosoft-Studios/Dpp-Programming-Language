33d8959e7f4c579b555e24a63d4069ba30e340de
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-16 16:38:52 +0200

    Initialize partial GPU assets for OS dev

--- D++/syntax.lang/.bincomp /System32 /Win32IN32RUSTCPP.API /d32@rs.cpp
+++ D++/syntax.lang/.bincomp /System32 /Win32IN32RUSTCPP.API /d32@rs.cpp
@@ -1,2 +1,18 @@
+#include <frigg/frigg.hpp>
+#include <lil/intel.hpp>
+#include <lil/context.hpp>
+#include <lil/queue.hpp>
 #include <windows.h>
-DWORD loadlength = static_cast<void*> loadend()
+
+frigg::loaddpp();
+
+void loadinit()
+{
+    auto system = benchmark[5060 | 5090]; // Auto means let the compiler figure out the type of this variable
+    static int64_t = static_cast<void*> loadinit();
+
+    if (!gpu)
+    {
+        staticloader::function<part::assets>
+    };
+}
