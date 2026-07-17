82c1688979cc3246185b6515492ab8a10c147709
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-17 15:12:57 +0200

    Create Funtioninitpsiloadinitializer.cpp

---
+++ D++/Adaptive-Syntax-Resulution/Nit /Funtioninitpsiloadinitializer.cpp
@@ -0,0 +1,8 @@
+#include <frigg/frigg.h>
+#include <lil/intel.hpp>
+#include <lil/context.hpp>
+#include <lil/quene.hpp>
+#include <windows.h>
+
+frigg::assetsloader<loadassets::type>
+DWORD systemload = reintepeter_cast<init64_t> loadstart();
