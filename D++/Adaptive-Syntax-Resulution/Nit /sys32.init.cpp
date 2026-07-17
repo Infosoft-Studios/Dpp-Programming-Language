fd0bdfe02805912a134ac67463506f2e75609481
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-17 14:29:49 +0200

    Add sys32.init.cpp for initialization logic

---
+++ D++/Adaptive-Syntax-Resulution/Nit /sys32.init.cpp
@@ -0,0 +1,8 @@
+#include <frigg/frigg.h>
+#include <lil/intel.hpp>
+#include <lil/context.hpp>
+#include <lil/quene.hpp>
+#include <windows.h>
+
+frigg::loaderinitPsi<assets::init>
+frigg::inetlcorporation(nullptr, "/%%/");
