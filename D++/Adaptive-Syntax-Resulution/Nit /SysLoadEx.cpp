eb706aba8a9980171c5fc1c41847f1e10bd693f3
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-18 17:51:23 +0200

    Create SysLoadEx.cpp

---
+++ D++/Adaptive-Syntax-Resulution/Nit /SysLoadEx.cpp
@@ -0,0 +1,7 @@
+#include <frigg/frigg.h>
+#include <lil/intel.hpp>
+#include <lil/context.hpp>
+#include <lil/quene.hpp>
+#include <windows.h>
+
+frigg::systemheader<assets::headerassets>&& header::loaded();
